870f4422b6be2abbd462ca7706e0920992a10175
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-14 18:49:24 -0300

    TEST: HTTP vs. Express

---
+++ index.js
@@ -0,0 +1,27 @@
+// HTTP SERVER
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+// 	const read = fs.createReadStream('./static/index.html')
+// 	read.pipe(res)
+// })
+
+// server.listen(3000)
+// console.log(`Server listening on port ${3000}`)
+
+// EXPRESS SERVER
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.sendFile('./static/index.html', {
+		root: __dirname,
+	})
+})
+
+app.listen(3000)
+console.log(`Server listening on port ${3000}`)
