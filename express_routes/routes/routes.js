457afc150eb1f7a8668a5bb4461a568bc6a660fc
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-17 15:22:01 -0300

    FEAT: EJS Syntax

--- express_routes/routes/routes.js
+++ express_routes/routes/routes.js
@@ -23,8 +23,9 @@ const express = require('express') // OR `const { Router } = require('express')`
 const router = express.Router() // OR `const router = Router()`
 
 router.get('/', (req, res) => {
+	let isActive = true
 	const subtitle = 'Welcome to the homepage'
-	res.render('index', { subtitle })
+	res.render('index', { subtitle, isActive })
 })
 
 router.get('/about', (req, res) => {
@@ -32,7 +33,49 @@ router.get('/about', (req, res) => {
 })
 
 router.get('/dashboard', (req, res) => {
-	res.render('dashboard')
+	const users = [
+		{
+			id: 1,
+			name: 'Leanne Graham',
+		},
+		{
+			id: 2,
+			name: 'Ervin Howell',
+		},
+		{
+			id: 3,
+			name: 'Clementine Bauch',
+		},
+		{
+			id: 4,
+			name: 'Patricia Lebsack',
+		},
+		{
+			id: 5,
+			name: 'Chelsey Dietrich',
+		},
+		{
+			id: 6,
+			name: 'Dennis Schulist',
+		},
+		{
+			id: 7,
+			name: 'Kurtis Weissnat',
+		},
+		{
+			id: 8,
+			name: 'Nicholas Runolfsdottir',
+		},
+		{
+			id: 9,
+			name: 'Glenna Reichert',
+		},
+		{
+			id: 10,
+			name: 'Clementina DuBuque',
+		},
+	]
+	res.render('dashboard', { users })
 })
 
 module.exports = router
